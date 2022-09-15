b42d2d934fc8f3daac64bd9b28225a85a6b85e56
ozancelikk0808 <ozancelikk0808@outlook.com>
2022-09-15 15:40:03 +0300

    Add project files.

---
+++ DatabaseLoggerServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class DatabaseLoggerServices : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veri Tabanına loglandı");
+        }
+    }
+}
---
+++ FileLoggerServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP3
+{
+    class FileLoggerServices : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Dosya loglandı");
+        }
+    }
+}
